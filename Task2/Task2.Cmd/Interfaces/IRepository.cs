b241f0bd47b92291eb4aa4df0369aa611cb65708
AndreyDonets <68755934+AndreyDonets@users.noreply.github.com>
2021-10-04 23:09:22 +0300

    added interfeces IRepository and IUnitOfWork

---
+++ Task2/Task2.Cmd/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Task2.Cmd.Models;
+
+namespace Task2.Cmd.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T GetById(int id);
+        List<T> GetList();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Task2/Task2.Cmd/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Task2.Cmd.Models;
+
+namespace Task2.Cmd.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Visitor> VisitorRepository { get; }
+        IRepository<HotelRoom> HotelRoomRepository { get; }
+        IRepository<InternalHotelInformation> InternalHotelInformation { get; }
+
+        void Save();
+    }
+}
